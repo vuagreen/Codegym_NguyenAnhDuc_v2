09f3c0d1c2c0a3a9dc807fdf4cfb5c486486082e
vuagreen <vuagreenr5@gmail.com>
2020-01-09 23:28:48 +0700

    first commit

---
+++ Module1/BienVaKieuDuLieu/TH/KhaiBaoVaSuDungBien/btap1.js
@@ -0,0 +1,11 @@
+let f = 20.5;
+let b = true;
+let s = 'Hà Nội';
+let i = 10;
+document.write('i = ' + i);
+document.write('<br/>');
+document.write('f = ' + f);
+document.write('<br/>');
+document.write('b = ' + b);
+document.write('<br/>');
+document.write('s = ' + s);
